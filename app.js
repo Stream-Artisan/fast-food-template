13d115a099826f1cc915d634283aa2d1dca0882b
AliAbdullah1999 <alithedeveloper1999@gmail.com>
2025-07-02 02:52:55 +0500

    working

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxy } = require("./proxy");
+const { createServer } = require("http");
+const { createServer: createHttpsServer } = require("https");
+const fs = require("fs");
